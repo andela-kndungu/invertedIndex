4be1674983ae369e5c158a6a7265e9c4f9cb8314
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-03-14 11:27:12 +0300

    Add populate index tests

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -2,26 +2,55 @@
 describe('Read book data', function() {
 
   // Initialise object
-  var index;
+  var invertedIndex;
 
   beforeEach(function() {
 
-    index = new Index();
+    invertedIndex = new InvertedIndex();
   });
 
   // Ensure non empty object is returned by createIndex
   it('is defined', function() {
 
-    expect(index.createIndex('books.json')).toBeDefined();
+    expect(invertedIndex.createIndex('books.json')).toBeDefined();
   });
 
   it('is an object', function() {
 
-    expect(index.createIndex('books.json')).toEqual(jasmine.any(Object));
+    expect(invertedIndex.createIndex('books.json')).toEqual(jasmine.any(Object));
   });
 
   it('is not empty', function() {
 
-    expect(index.createIndex('books.json')).not.toEqual({});
+    expect(invertedIndex.createIndex('books.json')).not.toEqual({});
+  });
+});
+
+// Ensure index is createed after JSON loads
+describe('Populate index', function() {
+
+  // Initialise object
+  var invertedIndex;
+
+  beforeEach(function() {
+
+    invertedIndex = new InvertedIndex();
+    // Load and parse JSON
+    invertedIndex.createIndex('books.json');
+  });
+
+  it('is defined', function() {
+
+    expect(invertedIndex.index).toBeDefined();
+  });
+
+  it('is an object', function() {
+
+    expect(invertedIndex.index).toEqual(jasmine.any(Object));
+  });
+
+  it('is not empty', function() {
+
+    expect(invertedIndex.createIndex('books.json')).not.toEqual({});
   });
 });
